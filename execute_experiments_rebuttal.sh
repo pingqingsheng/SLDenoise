055d0068dc0120d122544c5fb9de14076fc0f30d
pingqingsheng <imzszhahahaha@gmail.com>
2022-07-27 18:59:24 -0700

    fix bug in baselines

--- execute_experiments_rebuttal.sh
+++ execute_experiments_rebuttal.sh
@@ -2,15 +2,15 @@ python -W ignore train_ours_binary_share_weighted_dev.py --seed 77 --gpus 0 --da
 python -W ignore train_ours_binary_share_weighted_dev.py --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
 
 cd baselines
-python -W ignore train_ours_temperature_scaling.py       --seed 77 --gpus 0 --dataset mnist --noise_type uniform --noise_strength 0.0 &
-python -W ignore train_cskd.py --seed 77 --gpus 0 --dataset mnist --noise_type uniform --noise_strength 0.0 &
-python -W ignore train_bayes_dropout.py --seed 77 --gpus 0 --dataset mnist --noise_type uniform --noise_strength 0.0 &
-python -W ignore train_ensemble.py --seed 77 --gpus 0 --dataset mnist --noise_type uniform --noise_strength 0.0 &
+python -W ignore run_temperature_scaling.py       --seed 77 --gpus 0 --dataset mnist --noise_type uniform --noise_strength 0.0 &
+python -W ignore run_cskd.py --seed 77 --gpus 0 --dataset mnist --noise_type uniform --noise_strength 0.0 &
+python -W ignore run_bayes_dropout.py --seed 77 --gpus 0 --dataset mnist --noise_type uniform --noise_strength 0.0 &
+python -W ignore run_ensemble.py --seed 77 --gpus 0 --dataset mnist --noise_type uniform --noise_strength 0.0 &
 
-python -W ignore train_ours_temperature_scaling.py       --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
-python -W ignore train_cskd.py --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
-python -W ignore train_bayes_dropout.py --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
-python -W ignore train_ensemble.py --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
+python -W ignore run_temperature_scaling.py       --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
+python -W ignore run_cskd.py --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
+python -W ignore run_bayes_dropout.py --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
+python -W ignore run_ensemble.py --seed 77 --gpus 1 --dataset cifar10 --noise_type uniform --noise_strength 0.0 &
 
 
 
